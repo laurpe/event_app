ac85785345ac944a1983253bf4645730b7ad64a9
Duyen Nguyen <hongduyen0705@gmail.com>
2022-05-25 10:35:52 +0300

    change to className in Footer, fetch data for eventShow

--- assets/components/EventShow.js
+++ assets/components/EventShow.js
@@ -4,19 +4,15 @@ import axios from "axios";
 
 const EventShow = () => {
   const [id, setId] = useState(useParams().id);
-  const [event, setEvent] = useState({
-    name: "",
-    short_description: "",
-    start_time: "",
-    end_time: "",
-  });
+  const [event, setEvent] = useState([]);
 
   useEffect(() => {
     axios
       .get(`http://api.hel.fi/linkedevents/v1/event/${id}`)
       .then(function (response) {
-        setEvent(response.data);
-        console.log(response.data);
+        const data = response.data;
+        setEvent(data);
+        console.log({ data });
       })
       .catch(function (error) {
         console.log(error);
@@ -25,11 +21,18 @@ const EventShow = () => {
 
   return (
     <div className="container border mt-5">
-      <h2>{event.name.fi}</h2>
+      {/* <img
+        src={
+          data?.images[0]?.url ||
+          "https://images.unsplash.com/photo-1472653431158-6364773b2a56?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469"
+        }
+        alt={data?.images[0]?.alt_text || "image name"}
+      /> */}
+      {/* <h2>{data.name.fi}</h2> */}
       <img src="" alt="" />
-      <p>{event.short_description.fi}</p>
-      <p>{event.start_time}</p>
-      <p>{event.end_time}</p>
+      <p>{data.short_description.fi}</p>
+      <p>{data.start_time}</p>
+      <p>{data.end_time}</p>
     </div>
   );
 };
--- assets/components/Footer.js
+++ assets/components/Footer.js
@@ -3,73 +3,73 @@ import React from "react";
 const Footer = () => {
   return (
     <footer
-      class="text-center text-white"
+      className="text-center text-white"
       style={{ backgroundColor: "#f1f1f1" }}
     >
-      <div class="container pt-4">
-        <section class="mb-4">
+      <div className="container pt-4">
+        <section className="mb-4">
           <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
             href="#!"
             role="button"
             data-mdb-ripple-color="dark"
           >
-            <i class="fab fa-facebook-f"></i>
+            <i className="fab fa-facebook-f"></i>
           </a>
 
           <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
             href="#!"
             role="button"
             data-mdb-ripple-color="dark"
           >
-            <i class="fab fa-twitter"></i>
+            <i className="fab fa-twitter"></i>
           </a>
 
           <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
             href="#!"
             role="button"
             data-mdb-ripple-color="dark"
           >
-            <i class="fab fa-google"></i>
+            <i className="fab fa-google"></i>
           </a>
 
           <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
             href="#!"
             role="button"
             data-mdb-ripple-color="dark"
           >
-            <i class="fab fa-instagram"></i>
+            <i className="fab fa-instagram"></i>
           </a>
 
           <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
             href="#!"
             role="button"
             data-mdb-ripple-color="dark"
           >
-            <i class="fab fa-linkedin"></i>
+            <i className="fab fa-linkedin"></i>
           </a>
 
           <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
             href="#!"
             role="button"
             data-mdb-ripple-color="dark"
           >
-            <i class="fab fa-github"></i>
+            <i className="fab fa-github"></i>
           </a>
         </section>
       </div>
 
       <div
-        class="text-center text-dark p-3"
+        className="text-center text-dark p-3"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
         © 2022 Summer Project:
-        <a class="text-dark" href="https://mdbootstrap.com/">
+        <a className="text-dark" href="https://mdbootstrap.com/">
           BCH
         </a>
       </div>

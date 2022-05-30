1e1e675cca36c7f98a14a97e40ddb43acd0bcc67
Duyen Nguyen <hongduyen0705@gmail.com>
2022-05-30 15:11:30 +0300

    redid search function

--- assets/components/EventList.js
+++ assets/components/EventList.js
@@ -4,32 +4,17 @@ import { Link } from "react-router-dom";
 
 const EventList = (props) => {
   // States
-  const [eventList, setEventList] = useState([]);
-  const [filteredData, setFilteredData] = useState([eventList]);
-  const [filterParam, setFilterParam] = useState("");
+  const [APIData, setAPIData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState("");
 
-  // handle search
-  const handleSearch = (e) => {
-    setSearch(e.target.value);
-  };
-
-  const filteredEvents = () => {
-    if (search === "") {
-      return eventList;
-    }
-    return eventList.filter((event) => {
-      return event.name.toLowerCase().includes(search.toLowerCase().trim());
-    });
-  };
-
+  // fetch events
   useEffect(() => {
-    // fetch events
     const fetchLocalEvents = async () => {
       setLoading(true);
       const response = await axios.get("/api/events");
-      setEventList(response.data);
+      setAPIData(response.data);
       setFilteredData(response.data);
       setLoading(false);
     };
@@ -37,9 +22,32 @@ const EventList = (props) => {
     fetchLocalEvents();
   }, []);
 
+  // handle search
+  const handleSearch = (searchValue) => {
+    // set search state to the searchValue
+    setSearch(searchValue);
+    if (search === "") {
+      setFilteredData(APIData);
+    } else {
+      // filter out APIData using filter method
+      const filteredEvents = APIData.filter((event) => {
+        return Object.values(event)
+          .join("")
+          .toLowerCase()
+          .includes(search.toLowerCase());
+      });
+      setFilteredData(filteredEvents);
+    }
+  };
+
   useEffect(() => {
-    const result = eventList.filter(({ name, price }) => price);
-  });
+    const result = APIData.filter(
+      ({ name, price }) =>
+        price === "free" ||
+        name.toLowerCase().includes(search.toLowerCase().trim())
+    );
+    setFilteredData(result);
+  }, []);
 
   if (loading) {
     return <p>Loading...</p>;
@@ -54,7 +62,7 @@ const EventList = (props) => {
         placeholder="Search"
         aria-label="Search"
         value={search}
-        onChange={handleSearch}
+        onChange={(e) => handleSearch(e.target.value)}
       />
       <div>
         <button>All</button>
@@ -62,37 +70,69 @@ const EventList = (props) => {
       </div>
 
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        {filteredEvents().map((event, _) => {
-          return (
-            <div key={event.id} className="col">
-              <div className="card shadow-sm">
-                <img
-                  className="card-img-top"
-                  src={
-                    // event?.images[0]?.url ||
-                    event?.image ||
-                    "https://images.unsplash.com/photo-1472653431158-6364773b2a56?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469"
-                  }
-                  // alt={event?.images[0].alt_text || "image name"}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{event.name}</h5>
+        {search.length > 1
+          ? filteredData.map((event) => {
+              return (
+                <div key={event.id} className="col">
+                  <div className="card shadow-sm">
+                    <img
+                      className="card-img-top"
+                      src={
+                        // event?.images[0]?.url ||
+                        event?.image ||
+                        "https://images.unsplash.com/photo-1472653431158-6364773b2a56?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469"
+                      }
+                      // alt={event?.images[0].alt_text || "image name"}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{event.name}</h5>
 
-                  <p>{event?.price}</p>
-                  <p className="text-danger">
-                    {props.dateTimeFormat(event.startDateTime)}
-                  </p>
-                  <Link
-                    to={`events/${event.id}`}
-                    className="btn btn-primary mx-1"
-                  >
-                    See more
-                  </Link>
+                      <p>{event?.price}</p>
+                      <p className="text-danger">
+                        {props.dateTimeFormat(event.startDateTime)}
+                      </p>
+                      <Link
+                        to={`events/${event.id}`}
+                        className="btn btn-primary mx-1"
+                      >
+                        See more
+                      </Link>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          );
-        })}
+              );
+            })
+          : APIData.map((event) => {
+              return (
+                <div key={event.id} className="col">
+                  <div className="card shadow-sm">
+                    <img
+                      className="card-img-top"
+                      src={
+                        // event?.images[0]?.url ||
+                        event?.image ||
+                        "https://images.unsplash.com/photo-1472653431158-6364773b2a56?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469"
+                      }
+                      // alt={event?.images[0].alt_text || "image name"}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{event.name}</h5>
+
+                      <p>{event?.price}</p>
+                      <p className="text-danger">
+                        {props.dateTimeFormat(event.startDateTime)}
+                      </p>
+                      <Link
+                        to={`events/${event.id}`}
+                        className="btn btn-primary mx-1"
+                      >
+                        See more
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
       </div>
     </div>
   );

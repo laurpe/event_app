ff7176df8c4da3f785b185ba4f64ba12d4afac9e
Kirsi Jokela <kirsijo@gmail.com>
2022-05-30 11:48:07 +0300

    login controller, security, authenticator setup, dealing with exceptions

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,26 +4,25 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
     private $username;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
+
+    #[ORM\Column(type: 'string')]
     private $password;
-
-    #[ORM\Column(type: 'array', nullable: true)]
-    private $events_created = [];
-
-    #[ORM\Column(type: 'array', nullable: true)]
-    private $favourites = [];
 
     public function getId(): ?int
     {
@@ -42,7 +41,39 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -54,27 +85,12 @@ class User
         return $this;
     }
 
-    public function getEventsCreated(): ?array
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
     {
-        return $this->events_created;
-    }
-
-    public function setEventsCreated(?array $events_created): self
-    {
-        $this->events_created = $events_created;
-
-        return $this;
-    }
-
-    public function getFavourites(): ?array
-    {
-        return $this->favourites;
-    }
-
-    public function setFavourites(?array $favourites): self
-    {
-        $this->favourites = $favourites;
-
-        return $this;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
